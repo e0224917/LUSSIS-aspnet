523286f3bbc297b1f25bfc122434781517eddaa0
Ton That Minh Nhat <e0224917@u.nus.edu>
2018-01-18 14:03:04 +0800

    Fix a dump mistake

--- LUSSIS/Models/Requisition.cs
+++ LUSSIS/Models/Requisition.cs
@@ -36,10 +36,8 @@ namespace LUSSIS.Models
         [StringLength(20)]
         public string Status { get; set; }
 
-        [ForeignKey("ApprovalEmpNum")]
         public virtual Employee ApprovalEmployee { get; set; }
 
-        [ForeignKey("RequisitionEmpNum")]
         public virtual Employee RequisitionEmployee { get; set; }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
