ef283df1dbfab6b4bc707310b0a16f8293212af7
e0231990 <e0231990@ISS-BOR3804>
2018-01-24 20:40:19 +0800

    Extension method to compare using contains

---
+++ LUSSIS/Extensions/StringExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LUSSIS.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool Contains(this string json, string prefix, StringComparison comp)
+        {
+            return source?.IndexOf(json, prefix) >= 0;
+        }
+    }
+}
