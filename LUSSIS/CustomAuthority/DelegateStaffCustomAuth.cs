352d4e7f50eae6c8ec4d098085cd1480c8e63c04
user <user@USER-PC>
2018-01-30 13:50:58 +0800

    ass lussis.constants

--- LUSSIS/CustomAuthority/DelegateStaffCustomAuth.cs
+++ LUSSIS/CustomAuthority/DelegateStaffCustomAuth.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using LUSSIS.Constants;
 using LUSSIS.Repositories;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
